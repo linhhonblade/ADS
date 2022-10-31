ee45621cea9291e049c688a778818a2f08ba645d
maiptn <mai.pham@on.net.vn>
2022-10-31 10:03:47 +0700

    fak me i forgot all sorting algorithm I ve learnt be4

---
+++ hackerrank/sorting/quick_sort/go.mod
@@ -0,0 +1,3 @@
+module github.com/linhhonblade/quick_sort
+
+go 1.19
---
+++ hackerrank/sorting/quick_sort/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low - 1
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	i++
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) {
+	if low < high {
+		arr, q := partition(arr, low, high)
+		quickSort(arr, low, q-1)
+		quickSort(arr, q+1, high)
+	}
+}
+
+func main() {
+	arr := []int{6, 3, 1, 7, 8, 2, 9, 4, 5}
+	quickSort(arr, 0, len(arr)-1)
+	fmt.Println(arr)
+}
