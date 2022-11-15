2bac98e1f5c85f6dc782a1edc2386fc522edf488
maiptn <mai.pham@on.net.vn>
2022-11-15 15:22:34 +0700

    [hackerrank] migratory bird

---
+++ hackerrank/migratory_bird/go.mod
@@ -0,0 +1,3 @@
+module github.com/linhhonblade/migratory_bird
+
+go 1.19
---
+++ hackerrank/migratory_bird/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func migratoryBirds(arr []int32) int32 {
+	// Write your code here
+	bird_freq := make(map[int32]int32)
+	for _, b := range arr {
+		bird_freq[b]++
+	}
+	var max_f, min_b int32
+	for b, f := range bird_freq {
+		if f > max_f {
+			max_f = f
+			min_b = b
+		}
+	}
+	for b, f := range bird_freq {
+		if f == max_f && b < min_b {
+			min_b = b
+		}
+	}
+	return min_b
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(arrCount); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := migratoryBirds(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
